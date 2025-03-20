4a372e30e472d6141c95d5254dbc77fa82c6fd8d
sahilauspicioussoft <sahil@auspicioussoft.com>
2025-03-20 10:02:01 +0530

    Add dashboard page layout with subscription reminders and recent users section

---
+++ src/app/company/dashboard/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+export default function Page() {
+  return (
+    <div className="flex flex-1 flex-col gap-4">
+      <h1 className="text-2xl font-bold block md:hidden">Dashboard</h1>
+      <div className="flex flex-1 flex-col gap-4">
+        <div className="grid grid-cols-12 gap-4 w-full">
+          <div className="col-span-12 lg:col-span-6 space-y-6 bg-[#1b2236] rounded-[12px] md:rounded-[20px] py-4 px-4 md:py-8 md:px-9 ">
+            <h2 className="text-white text-[20px] md:text-2xl font-bold mb-3">
+              Subscription Reminder
+            </h2>
+            {/* <SubscriptionReminderTable /> */}
+          </div>
+          <div className="col-span-12 lg:col-span-6 space-y-6 bg-[#1b2236] rounded-[12px] md:rounded-[20px] py-4 px-4 md:py-8 md:px-9">
+            <h2 className="text-white text-[20px] md:text-2xl font-bold mb-3">
+              Subscriptions Expiring Within One Week
+            </h2>
+            {/* <SubscriptionsOneWeekTable /> */}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-12 gap-4 w-full">
+          <div className="col-span-12  space-y-6 bg-[#1b2236] rounded-[12px] md:rounded-[20px] py-4 px-4 md:py-8 md:px-9">
+            <h2 className="text-white text-[20px] md:text-2xl font-bold mb-3">
+              Recent New Users
+            </h2>
+            {/* <RecentNewUsers /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
